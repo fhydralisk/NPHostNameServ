6ba555fe6f7751142e853060029f52901bc4e555
Fan Hangyu <fhy114@mails.tsinghua.edu.cn>
2016-08-24 19:24:36 +0800

    Minor code style fix

--- HostnameUpdater.py
+++ HostnameUpdater.py
@@ -75,7 +75,7 @@ class Host(object):
         ret = {
             "Status": self.status,
             "DNS": {k: {"hostname": format_time(v["DNS_HOSTNAME"]) if format_time else v["DNS_HOSTNAME"],
-                        "last_update": v["UPDATE_TIME"], "State":v["State"]}
+                        "last_update": v["UPDATE_TIME"], "State": v["State"]}
                     for k, v in self.dnss.items()},
 
             "Script": {k: {"last_update": format_timestamp(v["UPDATE_TIME"]) if format_time else v["UPDATE_TIME"],
